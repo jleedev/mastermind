5030b1aefecb52ebc57bd4ba0590fdc3d68905c0
Josh Lee <jleedev@gmail.com>
2009-02-02 05:10:50 -0500

    Moved hsl guy

--- mastergui.rb
+++ mastergui.rb
@@ -1,18 +1,5 @@
 require 'mastermodel'
 
-def hue2rgb h
-  h = (h.to_f / 60 + 1e-3) % 6
-  c = h % 1
-  case h
-  when 0..1: rgb(1.0, c,   0.0)
-  when 1..2: rgb(1-c, 1.0, 0.0)
-  when 2..3: rgb(0.0, 1.0, c  )
-  when 3..4: rgb(0.0, 1-c, 1.0)
-  when 4..5: rgb(c,   0.0, 1.0)
-  when 5..6: rgb(1.0, 0.0, 1-c)
-  end
-end
-
 SIZE=50
 
 Shoes.app :title => "Mastermind", :width => 300, :height => 550, :resizable => false do
@@ -25,6 +12,19 @@ Shoes.app :title => "Mastermind", :width => 300, :height => 550, :resizable => f
   @pending = @game.blank
 
   # PREPARE THE LIST OF COLORS
+
+  def hue2rgb h
+    h = (h.to_f / 60 + 1e-3) % 6
+    c = h % 1
+    case h
+    when 0..1: rgb(1.0, c,   0.0)
+    when 1..2: rgb(1-c, 1.0, 0.0)
+    when 2..3: rgb(0.0, 1.0, c  )
+    when 3..4: rgb(0.0, 1-c, 1.0)
+    when 4..5: rgb(c,   0.0, 1.0)
+    when 5..6: rgb(1.0, 0.0, 1-c)
+    end
+  end
 
   def colors n
     1.upto(n).collect {|i| hue2rgb 360*i/n}
